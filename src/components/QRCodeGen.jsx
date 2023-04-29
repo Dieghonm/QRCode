d63cb029dc4bd5b4e51cb07e5155e5a637fa10d1
Diegho <dieghonm@gmail.com>
2023-04-29 19:05:03 -0300

    cria botão

--- src/components/QRCodeGen.jsx
+++ src/components/QRCodeGen.jsx
@@ -4,20 +4,30 @@ import QRCode from "react-qr-code";
 
 function QRCodeGen() {
   const [input, setInput] = useState('')
+  const [qrText, setQrText] = useState('')
   const style = {
     display: "flex",
     flexDirection: 'column',
-    margin: '5em'
+    margin: '2em 20em',
+    border: '5px solid black',
+    width: '420px',
+
   }
   
   return (
     <div style={style}>
+      <h3 style={{margin: '5px 100px'}}>Gerador de QR Code</h3>
       <textarea
+        style={{width: '400px', height:'100px', margin: '5px'}}
         placeholder='Digite aqui'
         onChange={(e) => setInput(e.target.value)}
       />
+      <button 
+        style={{width: '400px', margin: '5px'}}
+      onClick={() => setQrText(input)}>Gerar QR Code</button>
       <QRCode
-        value={input}
+              style={{margin: '5px 10px 10px 70px'}}
+        value={qrText}
       />
     </div>
   );
